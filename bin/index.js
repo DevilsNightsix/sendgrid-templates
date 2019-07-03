bfe6158e41c689f4ac745f121c53ff066d977557
Sergey Bezhenyan <eeendi94@gmail.com>
2019-07-03 13:36:29 +0300

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+var initCmd = require('../lib/commands/init');
+var createTemplateCmd = require('../lib/commands/create-template');
+var attachTemplateCmd = require('../lib/commands/attach-template');
+var createVersionCmd = require('../lib/commands/create-version');
+
+var {
+  listLocalFn,
+  listFetchedFn
+} = require('../lib/commands/list');
+
+program.command('init').action(() => initCmd());
+
+program.command('list')
+  .alias('l')
+  .description('List all templates')
+  .option('-l, --local', 'list locally installed templates only')
+  .action((cmd) => cmd.local ? listLocalFn() : listFetchedFn());
+
+program.command('template-create <templateName>')
+  .alias('tc')
+  .description('Add new template to sendgrid')
+  .option('-p, --path <path>', 'path to template')
+  .option('-s, --subject <subject>', 'Email subject')
+  .option('-d, --data [data]', 'path to test data')
+  .action((templateName, cmd) => createTemplateCmd(templateName, cmd.path, cmd.subject, cmd.data))
+
+program.command('template-attach <templateId>')
+  .alias('ta')
+  .description('Attach local files to existent sendgrid template')
+  .option('-p, --path <path>', 'path to template')
+  .option('-s, --subject <subject>', 'Email subject')
+  .option('-d, --data [data]', 'path to test data')
+  .option('-v, --verison [verisonId]', 'update a specific version of template (when empty new version would be created)')
+  .action((templateId, cmd) => attachTemplateCmd(templateId, cmd.path, cmd.subject, cmd.data, cmd.verisonId))
+
+program.command('version-create <templateId>')
+  .alias('vc')
+  .description('create a new version for specific template')
+  .option('-p, --path <path>', 'path to template')
+  .option('-s, --subject <subject>', 'Email subject')
+  .option('-d, --data [data]', 'path to test data')
+  .action((templateId, cmd) => createVersionCmd(templateId, cmd.path, cmd.subject, cmd.data))
+
+program.parse(process.argv);
